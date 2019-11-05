40b27a14e9362bd1e3e00f54cd8b862487b572aa
mtsgi <oq@live.jp>
2019-11-06 06:29:46 +0900

    Add Results Index

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,11 @@
 class ResultsController < ApplicationController
 
   def index
-    @results1 = account.p1
-  
+    if account
+      @results1 = account.p1
+      @results2 = account.p2
+    else
+      redirect_to :root
+    end
   end
 end
