e5d5b77e3eca128ade5d29af035ce960f09c4f8c
Takahashi <ne300229@senshu-u.jp>
2020-11-18 00:32:42 +0900

    Revert "deckコントローラーを作成"
    This reverts commit b5c896548d5d3bf6586e67e39d640c042a3f0a1a.

--- app/controllers/deck_controller.rb
+++
@@ -1,2 +0,0 @@
-class DeckController < ApplicationController
-end
--- test/controllers/deck_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DeckControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
