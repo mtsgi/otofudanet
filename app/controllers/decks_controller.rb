df68e95d0a4ed8db467c48fe866df5046a682231
Takahashi <ne300229@senshu-u.jp>
2020-11-20 01:42:41 +0900

    デッキの新規登録機能を実装（UIは仮・バリデーションはまだ）

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -9,4 +9,23 @@ class DecksController < ApplicationController
   def show
     @cards = Card.all
   end
+
+  def new
+    @user = account
+    @deck = Deck.new(user_id: @user.id, status: 0)
+  end
+
+  def create
+    @deck = Deck.new(deck_params)
+    if @deck.save
+      redirect_to decks_path, notice: "デッキを登録しました。"
+    else
+      render "new"
+    end
+  end
+
+  private
+  def deck_params
+    params.require(:deck).permit(:user_id, :name, :slot1, :slot2, :slot3, :slot4, :slot5, :status)
+  end
 end
