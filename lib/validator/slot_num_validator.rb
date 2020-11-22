a417928fccc39d9df303caf37439ac4ba70dfb12
Takahashi <ne300229@senshu-u.jp>
2020-11-23 02:24:04 +0900

    コメもらったとこの修正

--- lib/validator/slot_num_validator.rb
+++ lib/validator/slot_num_validator.rb
@@ -4,7 +4,6 @@ class SlotNumValidator < ActiveModel::EachValidator
       record.__send__ "slot#{n}"
     end
     slots.combination(2).each do |n| #配列から２つずつ取り出して比較
-      puts "(#{n[0]},#{n[1]})" # for debug
       if n[0] == n[1]
         record.errors.add(attribute, "が重複しています")
         break
